00b15ac082696aceab701d26a72406214daaad18
srujanavemula <srujanavemula12@gmail.com>
2024-12-16 14:55:39 +0530

    primeexample

---
+++ PrimeExample.java
@@ -0,0 +1,9 @@
+public class PrimeExample {
+    public static void main(String[] args) {
+        int num = 29, count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) count++;
+        }
+        System.out.println(count == 2 ? "Prime" : "Not Prime");
+    }
+}
